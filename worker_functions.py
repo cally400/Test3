cad17c7aed84dd3aa6dc5075209d30e298258051
cally400 <battle.of.zajil@gmail.com>
2025-12-18 09:46:44 +0300

    Update worker_functions.py

--- worker_functions.py
+++ worker_functions.py
@@ -1,17 +1,14 @@
 from ichancy_api import IChancyAPI
 import time
+import datetime
 
 def keep_session_alive():
-    """
-    إبقاء جلسة iChancy نشطة.
-    - تتحقق من تسجيل الدخول كل 5 دقائق
-    - تعيد تسجيل الدخول إذا انتهت الجلسة
-    """
     api = IChancyAPI()
     while True:
         try:
             api.ensure_login()
-            print("✅ Session is active")
+            print(f"{datetime.datetime.now()} ✅ Session is active")
         except Exception as e:
-            print(f"❌ Error in keep_session_alive: {e}")
+            print(f"{datetime.datetime.now()} ❌ Error in keep_session_alive: {e}")
         time.sleep(300)  # كل 5 دقائق
+
